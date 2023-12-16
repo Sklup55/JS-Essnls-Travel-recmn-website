68f7ec54cea0a6123ca7e3e5a6fb3fbbd9d619b3
Sklup55 <85282385+Sklup55@users.noreply.github.com>
2023-12-16 12:00:11 +0530

    Update travel_recommendation.js

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -44,23 +44,24 @@ function loadPage(page) {
     }
 }
 
-function fetchRecommendations() {
-    const apiUrl = 'https://restcountries.com/v3.1/all';
+// Function to fetch recommendations from JSONPlaceholder for testing
+function fetchRecommendations(keyword = '') {
+    const apiUrl = `https://jsonplaceholder.typicode.com/posts?q=${keyword}`;
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            const recommendations = data.map(country => ({
-                name: country.name.common,
-                type: 'country',
-                image: country.flags.svg,
-                description: `Explore the beauty of ${country.name.common}.`
+            const recommendations = data.map(post => ({
+                name: post.title,
+                type: 'post',
+                description: post.body
             }));
 
             displayRecommendations(recommendations);
         })
         .catch(error => console.error('Error fetching data:', error));
 }
+
 
 function displayRecommendations(data) {
     const resultsContainer = document.getElementById('recommendations-results');
@@ -80,6 +81,7 @@ function displayRecommendations(data) {
     });
 }
 
+// Function to handle search button click
 function searchRecommendations() {
     const searchInput = document.getElementById('searchInput');
     const keyword = searchInput.value.toLowerCase();
@@ -89,10 +91,12 @@ function searchRecommendations() {
     }
 }
 
+// Function to clear results in the recommendations container
 function clearResults() {
     const resultsContainer = document.getElementById('recommendations-results');
     resultsContainer.innerHTML = '';
 }
+
 
 function displayTime() {
     const timeDisplay = document.getElementById('time-display');
